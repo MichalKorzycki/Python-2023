9b8f071b7873a668ab8528328bce5ab8d9c4bf34
Michal Korzycki <michal.korzycki@gmail.com>
2023-06-28 14:25:23 +0200

    Food items

---
+++ 18_Wstep_do_obiektowosci/food_items.py
@@ -0,0 +1,22 @@
+import csv
+from pprint import pprint
+
+
+class FoodItem:
+    def __init__(self, item_id, item_name, price):
+        self.id = int(item_id)
+        self.name = item_name
+        self.price = float(price)
+
+    def __repr__(self):
+        return f'FoodItem({self.id}, "{self.name}", {self.price})'
+
+
+food_list = []
+
+with open('foods.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        food_list.append(FoodItem(item_id=row['Food ID'], item_name=row['Food Item'], price=row['Price']))
+
+pprint(food_list)
