65efebd1a4614d514fc8b735bbe70707409bc7d9
Michal Korzycki <michal.korzycki@gmail.com>
2023-06-29 11:05:58 +0200

    sql

--- 17_Wykorzystanie_baz_danych/ddl/period.sql
+++ 17_Wykorzystanie_baz_danych/ddl/period.sql
@@ -23,7 +23,7 @@ SET default_table_access_method = heap;
 --
 -- Name: period_type; Type: TABLE; Schema: public; Owner: my_user
 --
-CREATE SEQUENCE dimension_order_seq;
+CREATE SEQUENCE public.dimension_order_seq;
 
 
 CREATE TABLE public.period_type (
