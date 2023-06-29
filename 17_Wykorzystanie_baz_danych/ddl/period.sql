3754ca96bf3019312bf00c8740a5b4037330bfbc
Michal Korzycki <michal.korzycki@gmail.com>
2023-06-29 08:39:10 +0200

    sql

--- 17_Wykorzystanie_baz_danych/ddl/period.sql
+++ 17_Wykorzystanie_baz_danych/ddl/period.sql
@@ -21,6 +21,60 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: period_type; Type: TABLE; Schema: public; Owner: julia
+--
+
+CREATE TABLE public.period_type (
+    id character varying(36) NOT NULL
+);
+
+
+ALTER TABLE public.period_type OWNER TO julia;
+
+--
+-- Data for Name: period_type; Type: TABLE DATA; Schema: public; Owner: julia
+--
+
+INSERT INTO public.period_type (id) VALUES ('PERIOD_TYPE_YEAR');
+INSERT INTO public.period_type (id) VALUES ('PERIOD_TYPE_QUARTER');
+INSERT INTO public.period_type (id) VALUES ('PERIOD_TYPE_MONTH');
+
+
+--
+-- Name: period_type period_type_pkey; Type: CONSTRAINT; Schema: public; Owner: julia
+--
+
+ALTER TABLE ONLY public.period_type
+    ADD CONSTRAINT period_type_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.8 (Ubuntu 13.8-1.pgdg18.04+1)
+-- Dumped by pg_dump version 13.8 (Ubuntu 13.8-1.pgdg18.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
 -- Name: period_dimension; Type: TABLE; Schema: public; Owner: julia
 --
 
